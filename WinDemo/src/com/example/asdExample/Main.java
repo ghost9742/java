46b2eabb735655f96e940feeb475c3febb98cdff
Nemanja Milosavljevic <milosavljevic.nemanja87@gmail.com>
2024-09-09 16:56:00 +0200

    scope visibility

---
+++ WinDemo/src/com/example/asdExample/Main.java
@@ -0,0 +1,10 @@
+package com.example.asdExample;
+
+public class Main {
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow("Java");
+        myWindow.setVisible(true);
+    }
+
+
+}
